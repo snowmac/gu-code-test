0440407f95c23279bb3812686a205040ca18d25f
Adam Bourg <adam.bourg@gmail.com>
2025-07-20 15:36:48 -0600

    scaffolding the project with logos and colors and such;

---
+++ src/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import React from 'react';
+
+const queryClient = new QueryClient();
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#6eb35f',
+    },
+    background: {
+      default: '#e6e6da',
+      paper: '#fff',
+    },
+  },
+});
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+}
